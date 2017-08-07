971cb5c14c3da160e36a36c5283917f12704614a
Daniel Kostro <kostro.d@gmail.com>
2017-08-07 15:50:30 +0200

    use webpack html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 'use strict';
 
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'app.js'
     },
-
+    devtool: 'source-map',
+    plugins: [new HtmlWebpackPlugin({
+        template: 'src/index.html'
+    })],
     module: {
         rules: [
             {
